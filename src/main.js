51a1f908b39cdcc8b0386271c5e21fcd1a0a993e
Jerry Tsai <purrup27@gmail.com>
2020-09-04 14:53:26 +0800

    remove unused component AppButton

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import './plugins/fontawesome'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import AppButton from './components/AppButton'
 import AppHeader from '@/components/AppHeader.vue'
 import '@/assets/css/tailwind.css'
 
-Vue.component('AppButton', AppButton)
 Vue.component('AppHeader', AppHeader)
 
 Vue.config.productionTip = false
