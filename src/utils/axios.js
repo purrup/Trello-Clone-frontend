cdcd7d4ab8aed9974f1b8b65602ee34d5bb5ea73
Vin <purrup27@gmail.com>
2021-04-13 21:18:35 +0800

    Set baseURL and add static.json

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,12 @@
 import Axios from 'axios'
 
 const options = {
-  baseURL: 'http://localhost:3000/',
+  baseURL: process.env.NODE_ENV ? 'https://trello-clone-backend-2021.herokuapp.com/' : 'http://localhost:3000/',
   // baseURL: 'https://trello-clone-backend-2021.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json'
   },
   withCredentials: true
-  // timeout: 30000
 }
 
 const instance = Axios.create(options)
