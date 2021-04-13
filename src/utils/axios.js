70677671fad2bb96fc3093351a365e0d77ab2d59
Vin <purrup27@gmail.com>
2021-04-14 01:01:22 +0800

    Fix bseeURL of axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const options = {
-  baseURL: process.env.NODE_ENV ? 'https://trello-clone-backend-2021.herokuapp.com/' : 'http://localhost:3000/',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://trello-clone-backend-2021.herokuapp.com/' : 'http://localhost:3000/',
   // baseURL: 'https://trello-clone-backend-2021.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json'
